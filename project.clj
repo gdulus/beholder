b09cb85bd643b117c98c8867937e018e5b4659af
gdulus <gdula.pawel@gmail.com>
2022-05-27 11:19:57 +0200

    licenses changes

--- project.clj
+++ project.clj
@@ -1,7 +1,5 @@
 (defproject beholder "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :description "API documentation search engine with microservice and K8S first approach.\n\n"
+  :license {:name "MIT License" :url "https://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :repl-options {:init-ns beholder.core})
