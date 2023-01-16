beeae31d90e7330dc614c177e27eb246b5a5092d
gdulus <gdula.pawel@gmail.com>
2023-01-16 21:56:13 +0100

    tests

--- project.clj
+++ project.clj
@@ -4,15 +4,15 @@
   :dependencies [
                  ;; -------------- base -----------------------------------
                  [org.clojure/clojure "1.11.1"]
-                 [prismatic/schema "1.4.0"]
+                 [prismatic/schema "1.4.1"]
                  [jarohen/chime "0.3.3"]
                  ;; -------------- k8s -----------------------------------
                  [nubank/k8s-api "0.1.2"]
-                 [io.fabric8/kubernetes-client "6.1.1"]
+                 [io.fabric8/kubernetes-client "6.3.1"]
                  ;; -------------- persistence ---------------------------
                  [cc.qbits/spandex "0.7.10"]
                  ;; -------------- web -----------------------------------
-                 [etaoin "1.0.38"]
+                 [etaoin "1.0.39"]
                  [compojure "1.6.3"]
                  [ring/ring-defaults "0.3.4"]
                  [ring-json-response "0.2.0"]
@@ -24,15 +24,16 @@
                  [org.clojure/data.json "2.4.0"]
 
                  ;; --------------- testing ------------------------------
-                 [clj-test-containers "0.7.2"]
+                 [clj-test-containers "0.7.4"]
+                 [ring/ring-mock "0.4.0"]
 
                  ;; --------------- logging ------------------------------
-                 [com.taoensso/timbre "5.2.1"]
+                 [com.taoensso/timbre "6.0.4"]
                  ]
   :plugins [
             [lein-environ "1.2.0"]                          ; make sure to change intellij repl run options to "run it with lein"
             [lein-ring "0.12.6"]
-            [lein-eftest "0.5.9"]]
+            [lein-eftest "0.6.0"]]
   :repl-options {:init-ns beholder.main}
   :test-selectors {:default (complement :unit)
                    :unit :unit}
---
+++ test/beholder/main_test.clj
@@ -0,0 +1,28 @@
+(ns beholder.main-test
+  (:require [beholder.main :as routes]
+            [beholder.model :as m]
+            [clj-test-containers.core :as tc]
+            [clojure.test :refer :all]
+            [ring.mock.request :as mock]))
+
+(defn start-container []
+  (->
+    (tc/create {:image-name    "docker.elastic.co/elasticsearch/elasticsearch:7.17.6"
+                :exposed-ports [9200]
+                :env-vars      {"discovery.type" "single-node"
+                                "ES_JAVA_OPTS"   "-Xms750m -Xmx750m"}
+                :wait-for      {:wait-strategy   :log
+                                :message         "\"message\": \"started\""
+                                :startup-timeout 15}
+                })
+    (tc/start!)))
+
+
+(deftest ^:unit your-handler-test
+  (with-redefs [beholder.repositories.config/get-beholder-config! (fn [] (m/map->BeholderConfig {}))
+                beholder.repositories.config/get-service-config (fn [id] (m/map->ServiceConfig {:openApiPath "/OAI/OpenAPI-Specification/main/examples/v3.0/api-with-examples.yaml"}))
+                beholder.repositories.k8s/get-service! (fn [id] (m/map->KubernetesService {:url "https://raw.githubusercontent.com"}))]
+    (is (= (routes/app-routes (mock/request :get "/service/12345678/openapi"))
+           {:status  200
+            :body    "Your expected result"}))))
+
