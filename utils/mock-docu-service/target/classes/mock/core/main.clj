700714419c4618aa0bced0258c9f49fcd94ccb0e
gdulus <gdula.pawel@gmail.com>
2023-01-22 12:21:00 +0100

    git ignore and cleanup

--- utils/mock-docu-service/target/classes/mock/core/main.clj
+++
@@ -1,1 +0,0 @@
-(do (clojure.core/ns mock.core.main (:gen-class)) (clojure.core/defn -main [] ((do (clojure.core/require (quote ring.server.leiningen)) (clojure.core/resolve (quote ring.server.leiningen/serve))) (quote {:ring {:handler mock.core/app, :nrepl {:start? true, :port 41111}, :open-browser? false, :stacktraces? false, :auto-reload? false, :auto-refresh? false}}))))
