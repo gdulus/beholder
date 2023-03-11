925c80bffe6fac4f253782761aba040cb7166277
gdulus <gdula.pawel@gmail.com>
2023-03-11 15:03:14 +0100

    New services

---
+++ src/beholder/services/async_job_service.clj
@@ -0,0 +1,5 @@
+(ns beholder.services.async-job-service
+  (:require
+   [schema.core :as s]))
+
+(defn start-job [name delay interval body])
--- src/beholder/services/k8s_service_doc.clj
+++ src/beholder/services/k8s_service_doc.clj
@@ -1,1 +1,9 @@
 (ns beholder.services.k8s-service-doc)
+
+(defn- trigger-inexing [since]
+   ;; - get all recently updated services
+   ;; - for each service get OpenaAPI doc and save it
+   ;; - for each service get AsyncAPI doc and save it
+   ;; - mark
+   ;;
+  )
